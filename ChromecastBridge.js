e10bd833755a57ceba50a06e5ff8c5d6253a4ba7
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:26:50 -0500

    fixed up 'push' validation

--- ChromecastBridge.js
+++ ChromecastBridge.js
@@ -183,7 +183,7 @@ ChromecastBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",
